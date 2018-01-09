dc3739334f449d3e7e974827a8be79cad4163662
Johann Lee <qinka@live.com>
2018-01-09 22:45:54 +0800

    update

--- learn-cuda/source/test-lb.cc
+++ learn-cuda/source/test-lb.cc
@@ -1,18 +1,29 @@
 
 #include <linear-combination.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char* argv[]) {
   int row = 2;
   int col = 3;
-  if(argc >= 2) {
-    sscanf(argv[0],"%d",&row);
-    sscanf(argv[1],"%d",&col);
+  if(argc >= 3) {
+    sscanf(argv[1],"%d",&row);
+    sscanf(argv[2],"%d",&col);
   }
   uint8_t* m1 = new uint8_t[row * col];
   uint8_t* m2 = new uint8_t[row * col];
   uint8_t* m3 = new uint8_t[row * col];
+
+  srand((int)time(0));
+
+  for (int i = 0; i < row; ++i)
+    for (int j = 0; j < col; ++j) {
+      m1[i*col + j] = rand() % 0xFF;
+      m2[i*col + j] = rand() % 0xFF;
+    }
+
 
   linear_combination(1,m1,1,m2,row,col,m3);
 
