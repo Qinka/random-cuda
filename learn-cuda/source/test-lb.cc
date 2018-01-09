f6c75cd064611ca15a10d8992976a0459f74318c
Johann Lee <qinka@live.com>
2018-01-09 21:08:44 +0800

    update

--- learn-cuda/source/test-lb.cc
+++ learn-cuda/source/test-lb.cc
@@ -8,6 +8,19 @@ int main() {
   uint8_t m3[] = {9,9,9,9,9,9};
 
   linear_combination(1,m1,1,m2,2,3,m3);
+
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 3; ++j)
+      printf("%u ",m3[i*3+j]);
+    printf("\n");
+  }
+  printf("+\n");
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 3; ++j)
+      printf("%u ",m3[i*3+j]);
+    printf("\n");
+  }
+  printf("=\n");
   for (int i = 0; i < 2; ++i) {
     for (int j = 0; j < 3; ++j)
       printf("%u ",m3[i*3+j]);
